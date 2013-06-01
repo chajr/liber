cde7bade118cfeaad5a6eef032bf3e45f5eb4a4b
Michal Adamiak <chajr@bluetree.pl>
2013-06-01 18:15:45 +0200

    added model to handling selecting all rooms

--- libs/QueryModels.php
+++ libs/QueryModels.php
@@ -2,7 +2,7 @@
 /**
  * @author chajr <chajr@bluetree.pl>
  * @package core
- * @version 0.1.0
+ * @version 0.2.0
  * @copyright chajr/bluetree
  */
 class Libs_QueryModels
@@ -10,6 +10,12 @@ class Libs_QueryModels
     static function getTerms ($currentTime)
     {
         $query = "SELECT * FROM terminy WHERE data_wyjazdu >= '$currentTime'";
+        return new Libs_Mysql($query);
+    }
+
+    static function getRooms ()
+    {
+        $query = "SELECT * FROM pokoje";
         return new Libs_Mysql($query);
     }
 }
