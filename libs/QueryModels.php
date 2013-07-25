4bbd89df9d8594e65da2ba8d80aa1670104234d1
chajr <chajr@bluetree.pl>
2013-07-25 20:29:56 +0200

    added queries to save reservation

--- libs/QueryModels.php
+++ libs/QueryModels.php
@@ -2,7 +2,7 @@
 /**
  * @author chajr <chajr@bluetree.pl>
  * @package core
- * @version 0.2.0
+ * @version 0.3.0
  * @copyright chajr/bluetree
  */
 class Libs_QueryModels
@@ -10,6 +10,7 @@ class Libs_QueryModels
     static function getTerms ($currentTime)
     {
         $query = "SELECT * FROM terminy WHERE data_wyjazdu >= '$currentTime'";
+
         return new Libs_Mysql($query);
     }
 
@@ -19,8 +20,35 @@ class Libs_QueryModels
         if ($id) {
             $query = "WHERE id = '$id'";
         }
-        
+
         $query = "SELECT * FROM pokoje $query";
+        return new Libs_Mysql($query);
+    }
+
+    static function saveReservation(
+        $imie, $nazwisko, $od, $do, $mail, $telefon, $ulica, $numer, $miasto,
+        $kod, $opcje
+    ){
+        $query = "INSERT INTO
+            rezerwacje
+            (imie, nazwisko, od, do, mail, telefon, ulica, numer, miasto, kod, opcje)
+            VALUES
+            ('$imie', '$nazwisko', '$od', '$do', '$mail', '$telefon', '$ulica',
+             '$numer', '$miasto', '$kod', '$opcje')
+        ";
+
+        return new Libs_Mysql($query);
+    }
+
+    static function saveTerm($roomId, $from, $to)
+    {
+        $query = "INSERT INTO 
+            terminy
+            (id_pokoje, data_przyjazdu, data_wyjazdu)
+            VALUES
+            ('$roomId', '$from', '$to')
+        ";
+
         return new Libs_Mysql($query);
     }
 }
