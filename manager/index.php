6ea9a1b3766d4e985012c3b4106a2d959df8324e
Michal Adamiak <chajr@bluetree.pl>
2013-08-19 20:37:48 +0200

    make admin panel (issue #4)
    added some base files for admin panel

---
+++ manager/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * main file that loads all required libraries, start core class and display required content
+ *
+ * @author chajr <chajr@bluetree.pl>
+ * @package core
+ * @version 0.1.0
+ * @copyright chajr/bluetree
+ */
+require_once '../libs/admin/Core.php';
+require_once '../libs/admin/QueryModels.php';
+require_once '../libs/Connection.php';
+require_once '../libs/Mysql.php';
+require_once '../libs/Render.php';
+require_once '../libs/Valid.php';
+require_once '../libs/Xml.php';
+require_once '../libs/QueryModels.php';
+require_once '../libs/phpmailer/class.phpmailer.php';
