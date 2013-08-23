7bd911bd1958d1e28777243bfe86ecf411f56377
Michal Adamiak <chajr@bluetree.pl>
2013-08-23 18:40:36 +0200

    added base path constant, fixed base paths fo libraries, running core library and handling of exception

--- manager/index.php
+++ manager/index.php
@@ -4,15 +4,28 @@
  *
  * @author chajr <chajr@bluetree.pl>
  * @package core
- * @version 0.1.0
+ * @version 0.2.0
  * @copyright chajr/bluetree
  */
-require_once '../libs/admin/Core.php';
-require_once '../libs/admin/QueryModels.php';
-require_once '../libs/Connection.php';
-require_once '../libs/Mysql.php';
-require_once '../libs/Render.php';
-require_once '../libs/Valid.php';
-require_once '../libs/Xml.php';
-require_once '../libs/QueryModels.php';
-require_once '../libs/phpmailer/class.phpmailer.php';
+$path = str_replace('/manager', '', dirname(__FILE__));
+define('BASE_PATH', $path);
+require_once BASE_PATH . '/libs/Core.php';
+require_once BASE_PATH . '/libs/admin/Core.php';
+require_once BASE_PATH . '/libs/QueryModels.php';
+require_once BASE_PATH . '/libs/admin/QueryModels.php';
+require_once BASE_PATH . '/libs/Connection.php';
+require_once BASE_PATH . '/libs/Mysql.php';
+require_once BASE_PATH . '/libs/Render.php';
+require_once BASE_PATH . '/libs/Valid.php';
+require_once BASE_PATH . '/libs/Xml.php';
+require_once BASE_PATH . '/libs/QueryModels.php';
+require_once BASE_PATH . '/libs/phpmailer/class.phpmailer.php';
+try{
+    $core = new Libs_Admin_Core();
+    echo $core->display();
+}catch (Exception $exception){
+    echo '<div class="error">
+            <i class="icon-error-alt"></i>
+            ' . $exception->getMessage() . '
+        </div>';
+}
