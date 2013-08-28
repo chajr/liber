b4b52a577db85bb4898ca90556bd691b6736266f
Michal Adamiak <chajr@bluetree.pl>
2013-08-28 18:16:39 +0200

    make admin panel (issue #4) changed Libs_Admin_Loger to newer version 1.1.1

--- libs/admin/Loger.php
+++ libs/admin/Loger.php
@@ -2,41 +2,56 @@
 /**
  * @author chajr <chajr@bluetree.pl>
  * @package admin
- * @version 1.0.0
+ * @version 1.1.1
  * @copyright chajr/bluetree
  */
 class Libs_Admin_Loger
 {
+    /**
+     * set user as logged in, and save his option in session
+     * @param integer $uid
+     * @param string $options user options in eg format 00101100
+     * @param string $group
+     */
     public static function logOn($uid, $options, $group)
     {
-        $code                               = self::code();
-        $_SESSION['log_class']['log']       = TRUE;
-        $_SESSION['log_class']['uid']       = $uid;
-        $_SESSION['log_class']['code']      = $code;
-        $_SESSION['log_class']['options']   = $options;
-        $_SESSION['log_class']['group']     = $group;
-        $_SESSION['log_class']['time']      = time() + 60*60;
+        $code                             = self::_code();
+        $_SESSION['log_class']['log']     = TRUE;
+        $_SESSION['log_class']['uid']     = $uid;
+        $_SESSION['log_class']['code']    = $code;
+        $_SESSION['log_class']['options'] = $options;
+        $_SESSION['log_class']['group']   = $group;
+        $_SESSION['log_class']['time']    = time() + 60*60;
     }
 
-    public static function logOut()
+    /**
+     * destroy user information in session
+     */
+    public static function logOff()
     {
         $_SESSION['log_class'] = array();
         unset($_SESSION['log_class']);
     }
 
-    public static function verification()
+    /**
+     * check that user is logged in
+     *
+     * @return bool
+     * @throws LibraryException
+     */
+    public static function verifyUser()
     {
-        if (!isset($_SESSION['log_class']['log']) ||
-            !isset($_SESSION['log_class']['uid']) ||
-            !isset($_SESSION['log_class']['code']) ||
-            !isset($_SESSION['log_class']['options']) ||
-            !isset($_SESSION['log_class']['group']) ||
-            !isset($_SESSION['log_class']['time']) ||
-            !$_SESSION['log_class']['log']) {
-
+        if (   !isset($_SESSION['log_class']['log'])
+            || !isset($_SESSION['log_class']['uid'])
+            || !isset($_SESSION['log_class']['code'])
+            || !isset($_SESSION['log_class']['options'])
+            || !isset($_SESSION['log_class']['group'])
+            || !isset($_SESSION['log_class']['time'])
+            || !$_SESSION['log_class']['log']
+        ){
             return FALSE;
         } else {
-            if ($_SESSION['log_class']['code'] === self::code()) {
+            if ($_SESSION['log_class']['code'] === self::_code()) {
                 if ($_SESSION['log_class']['options']{0} === '0') {
                     throw new LibraryException('no_reg');
                 }
@@ -51,7 +66,7 @@ class Libs_Admin_Loger
 
                 $_SESSION['log_class']['time'] = time() + 60*60;
                 @session_regenerate_id();
-                $_SESSION['log_class']['code'] = self::code();
+                $_SESSION['log_class']['code'] = self::_code();
 
                 return TRUE;
             } else {
@@ -60,11 +75,16 @@ class Libs_Admin_Loger
         }
     }
 
-    private static function code()
+    /**
+     * return special code to verify user
+     *
+     * @return string
+     */
+    protected static function _code()
     {
         $client     = $_SERVER['HTTP_USER_AGENT'];
         $ip         = $_SERVER['REMOTE_ADDR'];
-        $code       = hash(
+        $code        = hash(
             'sha256',
             $client . $ip
         );
