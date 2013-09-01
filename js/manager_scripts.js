2942dbb5de2ce53c6e07f10b1176cc5b5dab3d26
Michal Adamiak <chajr@bluetree.pl>
2013-09-01 21:27:59 +0200

    make admin panel (issue #7) fixed class names for remove term script handling

--- js/manager_scripts.js
+++ js/manager_scripts.js
@@ -1,15 +1,15 @@
 $(document).ready(function()
 {
     var termToRemove;
-    $('.remove_reservation').click(function()
+    $('.remove_term').click(function()
     {
         termToRemove = $(this).data('id');
     });
-    $('.remove_reservation_modal').click(function()
+    $('.remove_term_modal').click(function()
     {
         window.location.href = '?page=remove_term&id=' + termToRemove;
     });
-    $('.remove_reservation_cancel').click(function()
+    $('.remove_term_cancel').click(function()
     {
         termToRemove = null;
     });
